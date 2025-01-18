049f6f21b56b052c546d63821b1239285f1684cf
Sadat Mahmud <sadatmahmud02@gmail.com>
2025-01-18 22:55:43 +0600

    added all url

--- twitter/tweet/urls.py
+++ twitter/tweet/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tweets/', views.tweet_list, name='tweet_list'),
+    path('tweets/create/', views.tweet_create, name='tweet_create'),
+    path('tweets/<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('tweets/<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
 ]
